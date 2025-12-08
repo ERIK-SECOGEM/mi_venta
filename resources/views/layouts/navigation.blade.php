ed79a755237ad4d16899017579edf4fabb801758
Erik Corral <erik.secogem@outlook.com>
2025-12-07 20:59:03 -0600

    Modificar el menú responsivo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -73,6 +73,10 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            
+            <x-responsive-nav-link :href="route('vehicle.index')" :active="request()->routeIs('vehicle.index')">
+            {{ __('Mis vehículos') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
