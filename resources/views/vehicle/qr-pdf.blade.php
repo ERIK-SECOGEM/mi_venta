34e1f1edb7319debda1f139e30ab4c4783a2b7d7
Erik Corral <erik.secogem@outlook.com>
2026-03-08 07:49:00 -0600

    Modificar imagen principal de la ficha

--- resources/views/vehicle/qr-pdf.blade.php
+++ resources/views/vehicle/qr-pdf.blade.php
@@ -15,7 +15,7 @@
 <p><strong>Año:</strong> {{ $vehiculo->anio }}</p>
 
 @if ($vehiculo->images)
-    <img src="{{ public_path('storage/' . $vehiculo->images->first()->path) }}" alt="Vehículo" width="100" height="150">
+    <img src="{{ public_path('storage/' . $vehiculo->images->first()->path) }}" alt="Vehículo" width="200" height="150">
 @endif
 
 <h3>Escanea el QR</h3>
